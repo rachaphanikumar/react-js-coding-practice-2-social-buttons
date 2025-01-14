b68fd2746d7355c8a7230cf4c155321b1673349f
rachaphanikumar <rachaphanikumar12@gmail.com>
2025-01-14 21:43:08 +0530

    index.js

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const Button = props => {
-  //  Write your code here.
-  const {name} = props
-  return <button className='message'>{name}</button>
+  const {className, buttonText} = props
+
+  return <button className={`button ${className}`}>{buttonText}</button>
 }
 
 const element = (
-  //  Write your code here.
-  <div className='background'>
-    <h1 className='heading'>Social Buttons</h1>
-    <div>
-      <Button className='button1' name='Like' />
-      <Button className='button2' name='Comment' />
-      <Button className='button3' name='Share' />
+  <div className='social-buttons-container'>
+    <div className='content-container'>
+      <h1 className='heading'>Social Buttons</h1>
+      <div className='buttons-container'>
+        <Button buttonText='Like' className='like-button' />
+        <Button buttonText='Comment' className='comment-button' />
+        <Button buttonText='Share' className='share-button' />
+      </div>
     </div>
   </div>
 )
 
 ReactDOM.render(element, document.getElementById('root'))
+
